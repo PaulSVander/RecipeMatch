729c43c5b34f5f97d61ebb88794de1ec68199526
Paul Vander <paulsvander@gmail.com>
2021-10-27 19:37:09 -0700

    add new recipe interface

--- data.py
+++ data.py
@@ -1,9 +1,23 @@
 import ingredient
+import recipe
 
 x = ingredient.Ingredient('banana', 20, 'lbs')
 y = ingredient.Ingredient('sugar', 30, 'oz')
 z = ingredient.Ingredient('salt', 50, 'g')
 
+a = recipe.Recipe('Pie')
+a.add_ingredient(x, 2)
+a.add_ingredient(y, 5)
+
+b = recipe.Recipe('Salad')
+b.add_ingredient(y, 2)
+b.add_ingredient(z, 1)
+
+c = recipe.Recipe('soup')
+c.add_ingredient(a, 1)
+c.add_ingredient(b,2)
+c.add_ingredient(c, 3)
+
 ingredients_list = [x, y, z]
-recipes_list = []
+recipes_list = [a, b, c]
 
